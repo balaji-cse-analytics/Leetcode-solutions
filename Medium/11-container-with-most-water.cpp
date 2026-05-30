84ab0577a6b25f303ef600bdf4b1c46ca6cb44ea
Balaji <singhbalaji481@gmail.com>
2026-05-30 21:06:44 +0530

    Add #2956 find common elements between two arrays Easy)

--- Medium/11-container-with-most-water.cpp
+++ Medium/11-container-with-most-water.cpp
@@ -3,7 +3,7 @@ public:
     int maxArea(vector<int>& height) {
         int left=0;
         int right=height.size()-1;
-        int max=0;
+        int maxi=0;
         
         while(left<right){
             int area=(right-left)*(min(height[left],height[right]));
@@ -13,10 +13,10 @@ public:
             else{
                 right--;
             }
-            if(area>max){
-                max=area;
+            if(area>maxi){
+                maxi=area;
             }
         }
-        return max;
+        return maxi;
     }
 };
